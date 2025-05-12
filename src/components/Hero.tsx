3a9f0511d668f24ee9a2d369f53478d6322e9607
eniverz <mspt5871@protonmail.com>
2025-05-13 07:03:08 +0800

    rename and CV

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 </div>
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
-                        Hi, I'm <span className={"text-[#915eff]"}>eniverz</span>
+                        Hi, I'm <span className={"text-[#915eff]"}>Eniverz</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                         I develop Embodied AI/Computer Vision program, web applications and games.
